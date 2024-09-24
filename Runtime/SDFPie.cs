fb9256fa837c55bc2e151503ad221f21965566fe
AAAYaKo <aaayakoorovka@gmail.com>
2024-09-24 14:45:32 +0700

    fix sdf arc and ring thicker than expected

--- Runtime/SDFPie.cs
+++ Runtime/SDFPie.cs
@@ -4,14 +4,13 @@
 * https://github.com/kirevdokimov/Unity-UI-Rounded-Corners/blob/master/UiRoundedCorners/Editor/ImageWithIndependentRoundedCornersInspector.cs
 **/
 
-using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 namespace TLab.UI.SDF
 {
-	public class SDFPie : SDFUI
+	public class SDFPie : SDFCircleBased
 	{
 #if UNITY_EDITOR
 		[MenuItem("GameObject/UI/SDFUI/SDFPie", false)]
@@ -22,33 +21,5 @@ namespace TLab.UI.SDF
 #endif
 
 		protected override string SHADER_NAME => "Hidden/UI/SDF/Pie/Outline";
-
-		[Range(0, 1), SerializeField]
-		private float m_fillAmount = 0.5f;
-
-		public static readonly int PROP_RADIUSE = Shader.PropertyToID("_Radius");
-		public static readonly int PROP_THETA = Shader.PropertyToID("_Theta");
-
-		public float fillAmount
-		{
-			get => m_fillAmount;
-			set
-			{
-				if (m_fillAmount != value)
-				{
-					m_fillAmount = value;
-
-					SetAllDirty();
-				}
-			}
-		}
-
-		protected override void UpdateMaterialRecord()
-		{
-			base.UpdateMaterialRecord();
-
-			_materialRecord.SetFloat(PROP_THETA, m_fillAmount * Mathf.PI);
-			_materialRecord.SetFloat(PROP_RADIUSE, minSize * 0.5f);
-		}
 	}
 }
